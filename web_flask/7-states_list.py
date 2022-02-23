b9933be7975d0ed623d02b1bb8491c98d27bbc6f
krummitz <amidbidee@gmail.com>
2022-02-23 09:12:28 +0100

    updates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,76 +1,27 @@
 #!/usr/bin/python3
-"""Flask app config"""
-
-from flask import Flask, abort, render_template
-from models import *
+""" Starts a Flash Web Application """
 from models import storage
-
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    """say hello"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb')
-def hbnb():
-    """simply return hbnb"""
-    return "HBNB"
-
-
-@app.route('/c/<string:s>')
-def c(s):
-    """
-    replace '_' in string with space
-    """
-    new_s = s.replace("_", " ")
-    return "C {}".format(new_s)
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<string:s>')
-def python(s="is cool"):
-    """more replaces"""
-    new_s = s.replace("_", " ")
-    return "Python {}".format(new_s)
-
-
-@app.route('/number/<n>')
-def number(n):
-    """check if number"""
-    try:
-        n = int(n)
-        return "{} is a number".format(n)
-    except Exception:
-        abort(404)
-
-
-@app.route('/number_template/<int:n>')
-def number_template(n):
-    """render a template"""
-    return render_template('5-number.html', num=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def number_odd_or_even(n):
-    """render more template"""
-    return render_template('6-number_odd_or_even.html', num=n)
-
-
-@app.route('/states_list')
-def states_list():
-    """retrieves all states"""
-    return render_template('7-states_list.html',
-                           states=storage.all("State"))
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def teardown(err):
-    """remove all active db sessions"""
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
